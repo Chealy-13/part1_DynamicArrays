52fb340f2240eb8163954a20a98f5087ff05b474
Chealy-13 <chris.healy33@gmail.com>
2024-02-09 08:12:03 +0000

    defualt constructor added

--- src/main/java/utils/DynamicArrayList.java
+++ src/main/java/utils/DynamicArrayList.java
@@ -3,4 +3,9 @@ package utils;
 public class DynamicArrayList {
     private String[] data;
 
+    //A default constructor that takes no parameters and initialises the internal array to the standard initial
+    //capacity.
+    public DynamicArrayList() {
+        data = new String[10];
+    }
 }
