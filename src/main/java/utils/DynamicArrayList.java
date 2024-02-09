a4693d08b7ce99e4f4ba690580df3c125fe6c01b
Chealy-13 <chris.healy33@gmail.com>
2024-02-09 08:18:59 +0000

    size() added

--- src/main/java/utils/DynamicArrayList.java
+++ src/main/java/utils/DynamicArrayList.java
@@ -2,10 +2,19 @@ package utils;
 
 public class DynamicArrayList {
     private String[] data;
+    private int numOfElements;
 
     //A default constructor that takes no parameters and initialises the internal array to the standard initial
     //capacity.
     public DynamicArrayList() {
         data = new String[10];
     }
+
+    //A method called size() that takes no parameters & returns the number of elements currently stored in your list
+    public int size() {
+        return numOfElements;
+    }
+
+
+
 }
