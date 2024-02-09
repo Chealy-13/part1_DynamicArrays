7eacee165080b250dfd8f469746d4b77bb7aa7e2
Chealy-13 <chris.healy33@gmail.com>
2024-02-09 09:02:43 +0000

    indexOf() added

--- src/main/java/utils/DynamicArrayList.java
+++ src/main/java/utils/DynamicArrayList.java
@@ -25,5 +25,23 @@ public class DynamicArrayList {
         return data[pos];
     }
 
+    //A method called indexOf() that takes one parameter, a String to be found in the list. The method should
+    //return an int indicating the position at which the String could be found. If the String could not be found, an
+    //appropriate value should be returned to indicate this.
+    public int indexOf(String stringToFind) {
+        for (int i = 0; i < data.length; i++) {
+            if (stringToFind == data[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+//A method called add() that takes a single parameter, a String to be added. This String should be added to the
+//end of the list. If there is insufficient space in the list, the internal array should be grown, and the String added
+//then. Null values should be allowed. The method should return a boolean indicating if the String was added.
+
+
+
 
 }
